addc1942475cf529c2c4d47193c751096d1a4fc1
francoisquellec <francois.quellec@heig-vd.ch>
2019-10-03 16:40:50 +0200

    3 out of 6 done

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -8,28 +8,40 @@ import numpy as np
 
 def compute_mse_loss(y, tx, w):
     """Calculate the loss using mse"""
-    e = y - tx@w
-    return 1/(2*len(y)) * e@e.T
+    error = y - tx@w
+    loss = 1/(2*len(y)) * error.T@error
+    return loss
 
 def compute_mae_loss(y, tx, w):
     """Calculate the loss using mae"""
-    e = y - tx@w
-    return 1/(2*len(y)) * np.sum(e)
+    error = y - tx@w
+    loss = np.mean(np.abs(e))
+    return loss
 
 def compute_gradient(y, tx, w):
     """Compute the gradient."""
-    e = y - tx@w
-    return -1/len(y) * tx.T@e
+    error = y - tx@w
+    gradient = -1/len(y) * tx.T@error
+    return gradient
 
+def standardize(x):
+    """Standardize the original data set."""
+    mean_x = np.mean(x, axis=0)
+    x = x - mean_x
+    std_x = np.std(x, axis=0)
+    x = x / std_x
+    return x, mean_x, std_x
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """Linear regression using gradient descent"""
     w = initial_w
+    loss = -1
+
     for n_iter in range(max_iters):
         # Compute loss and gradient then update accordingly the weights
         loss = compute_mae_loss(y, tx, w)
         gradient = compute_gradient(y, tx, w)
-        w = w- gamma * gradient
+        w -= gamma * gradient
 
         # Print each iteration for debugging purpose
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
